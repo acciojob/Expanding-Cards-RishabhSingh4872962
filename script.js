d67cbd40ea4e50d64da5f6b6dd8e2b9f2a5a1011
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-03-01 09:48:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,16 +2,6 @@
 
 const arr=["panel-1"]
 
-function handleImg() {
-	const panelRemove=document.getElementById(arr[0]);
-	panelRemove.classList.remove("active");
-	
-	
- arr.pop();
-	 arr.push(this.event.target.id);
-	 const panelGet=document.getElementById(arr[0]);
-	 panelRemove.classList.add("active");
-	console.log(panelGet)
-	
+function handleImg() { 
 	
 }
